227573d1d95eab22b8e1ca418bb2fe0b46f6cb38
hatso <mmorpg3636@naver.com>
2021-09-15 15:47:55 +0900

    AI work commit 09/15

--- Beginner/Homework/week01/random_turtleway.py
+++ Beginner/Homework/week01/random_turtleway.py
@@ -1,30 +1,79 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 11 15:58:33 2021
+from turtle import *
+import random
 
-@author: kimhuiseong
-"""
-screen = turtle.Screen()
-screen.setup(1000,1000)
-screen.tracer(0,0)
+#%% target 난수 생성
+targetX = (random.randint(-10, 10)) * 10
+targetY = (random.randint(-10, 10)) * 10
 
-#%% list array setting
-walkers = list()
-n = 20
-for i in range(n):
-    walkers.append(turtle.Turtle())
-for i in range(n):
-    walkers[i].color((random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)))
+mypen=Turtle()
 
-#%% random_walk() method setting                 
-def random_walk():
-    for i in range(n):
-        angle = random.randint(0,3)*90
-        walkers[i].seth(angle)
-        walkers[i].fd(10)
-    screen.update()
-    screen.ontimer(random_walk,1000//20)
+#%% 울타리 그리기
+mypen.penup()
+mypen.pencolor("black")
+mypen.setposition(-100,-100)
+mypen.pendown()
+for x in range(4):
+    mypen.forward(200)
+    mypen.left(90)
+    
+#%% 출발 지점 표시
+mypen.penup()
+mypen.pen(pencolor="blue", pensize=10)
+mypen.setpos(0,0)
+mypen.pendown()
+mypen.setpos(0,0)
 
-#%% run
-random_walk()
+#%% 목표 지점 표시
+mypen.penup()
+mypen.pen(pencolor="red", pensize=10)
+mypen.setpos(targetX,targetY)
+mypen.pendown()
+mypen.setpos(targetX,targetY)
+
+#%% 거북이 설정
+mypen.penup()
+mypen.pen(pencolor="black", pensize=1)
+mypen.setpos(0,0)
+mypen.speed(10)
+mypen.shape("turtle")
+mypen.pendown()
+
+print("시작점 : ",mypen.pos())
+print("목표 : " + "[" + str(targetX) + "," + str(targetY) + "]")
+
+#%% 거북이 움직임 기록
+number=0
+
+while True:
+    posX = round(mypen.xcor())
+    posY = round(mypen.ycor())
+    
+    if posX == targetX and posY == targetY:
+        print("총"+str(number)+"의 움직임 발생")
+        break
+    
+    number += 1
+    rand = random.randint(0, 3)
+    
+    if(rand==0):
+        rotate = 0
+    elif(rand==1):
+        rotate = 90
+    elif(rand==2):
+        rotate = 180
+    else:
+        rotate = 270
+    mypen.seth(rotate)
+    
+#%% 울타리 범위 벋어나지 못하게 한다.
+
+    if(posX >= 100 and mypen.heading()==0): #동쪽
+        continue
+    elif(posY <= -100 and mypen.heading()==270): #남쪽
+        continue
+    elif(posX <= -100 and mypen.heading()==180): #북쪽
+        continue
+    elif(posY >= 100 and mypen.heading()==90): #서쪽
+        continue
+    else:
+        mypen.forward(10)
---
+++ Beginner/drawing_iris.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import plotly.express as px
+
+df = px.data.iris()
+fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width', color='species')
+fig.show(renderer="browser")
